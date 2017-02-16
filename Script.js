97cde1a5c678e8376711c6608102de20fd27acfc
Chris Moes <moescs@gmail.com>
2017-02-15 21:05:54 -0500

    Updated jQuery & highlight.js and converted links to // for https support

--- Script.js
+++ Script.js
@@ -1,23 +1,23 @@
-var csm; 
-(function (csm) { 
+var csm;
+(function (csm) {
 "use strict";
 var util;
 (function (util) {
 
-    util.loadJQuery = function (callback) {    
+    util.loadJQuery = function (callback) {
         //don't load if if we have it
         if (typeof $ !== 'undefined') {
             callback();
             return;
         }
-    
+
         //create script element
         var script = document.createElement('script');
-        script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js';
-        
-        //bind the event to the callback function 
+        script.src = '//code.jquery.com/jquery-3.1.1.slim.min.js';
+
+        //bind the event to the callback function
         script.onload = callback;
-    
+
         // add script to the head
         var head = document.getElementsByTagName('head')[0];
         // fire the loading
@@ -26,32 +26,32 @@ var util;
 
     //return: jqXHR
     util.getCachedScript = function (url, options) {
-      // allow user to set any option except for dataType, cache, and url
-      options = $.extend(options || {}, {
-        dataType: "script",
-        cache: true,
-        url: url
-      });
- 
-      // Use $.ajax() since it is more flexible than $.getScript
-      // Return the jqXHR object so we can chain callbacks
-      return $.ajax(options);
+        // allow user to set any option except for dataType, cache, and url
+        options = $.extend(options || {}, {
+            dataType: "script",
+            cache: true,
+            url: url
+        });
+
+        // Use $.ajax() since it is more flexible than $.getScript
+        // Return the jqXHR object so we can chain callbacks
+        return $.ajax(options);
     };
-    
+
 })(csm.util = util || {})
 
 var codeStyler;
 (function (codeStyler) {
     var imports = csm.util;
 
-    function styleCode() {  
+    function styleCode() {
         //For each pre element that has a code element...
         $('pre code').each(function(i, e) {
             if (!$(e).hasClass('highlighted_syntax')) {
                 $(e).addClass('highlighted_syntax');
                 hljs.highlightBlock(e);
             }
-        }); 
+        });
     }
 
     codeStyler.run = function () {
@@ -60,24 +60,23 @@ var codeStyler;
             var cssId = 'D-qnT2WAQYqbSgAAk9ywWQ';
             //add css if it's not here
             if ($('head link#'+cssId).length == 0) {
-                var highlightCss = $.parseHTML('<link'
+                var head = $('head');
+                $.parseHTML('<link'
                     +' id="' + cssId + '"'
                     +' rel="stylesheet"'
-                    +' href="http://yandex.st/highlightjs/7.4/styles/default.min.css"/>');
-                $('head').append(highlightCss);
-            
-                //customize for horizontal scrolling
-                var customCss = $.parseHTML('<style>'
+                    +' href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/styles/default.min.css"/>')
+                    .appendTo(head);
+                $.parseHTML('<style>'
                     +'pre code {'
+                        //customize for horizontal scrolling
                     	+'overflow-x: auto;'
                     	+'white-space: pre;'
                     	+'word-wrap: normal; }'
-                    +'</style>');
-                $('head').append(customCss);
+                    +'</style>')
+                    .appendTo(head);
             }
-            
             //Style code
-            imports.getCachedScript("http://yandex.st/highlightjs/7.4/highlight.min.js")
+            imports.getCachedScript("//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/highlight.min.js")
                 //and finally what we all came here for...
                 .done(function() { styleCode(); });
         }));
